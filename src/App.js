beac367149f4a4aed113fda646ec6afd0acc4abb
Jazthedev <joelpepa2@gmail.com>
2023-03-07 18:17:18 +0100

    add development navbar of chatrooms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { Link,Outlet } from "react-router-dom"
 function App() {
-    return <div>App</div>
+    return <div>
+        <Link to={"/auth/login"}> <button>Login btn</button></Link>
+        <Link to={"/auth/signup"}><button>Signup btn</button></Link>
+        <Outlet />
+        </div>
 }
 
 export default App
