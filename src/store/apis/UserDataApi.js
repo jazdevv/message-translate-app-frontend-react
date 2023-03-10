75a7aaa53259f719cca45544e947652ec72369a7
Jazthedev <joelpepa2@gmail.com>
2023-03-10 21:12:28 +0100

    add RTQ Query for logged user data

---
+++ src/store/apis/UserDataApi.js
@@ -0,0 +1,15 @@
+import { createApi,fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { useSelector } from 'react-redux'
+
+const UserDataApi = createApi({
+    reducerPath:"UserData",
+    baseQuery:fetchBaseQuery({baseUrl:'http://127.0.0.1:3001',credentials:"include"}),
+    endpoints: (builder)=>({
+        getLoggUserData: builder.query({
+            query:()=>'/auth/me'
+        })
+    })
+})
+
+export const {useGetLoggUserDataQuery} = UserDataApi
+export {UserDataApi}
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
 import {serverUrlSlice} from './slices/serverUrl'
 import { chatRoomsSlice } from './slices/chatRoomsSlice'
+import { UserDataApi, useGetLoggUserDataQuery } from './apis/UserDataApi'
+import { setupListeners } from '@reduxjs/toolkit/dist/query'
 
 const store = configureStore({
   reducer: {
     serverUrl: serverUrlSlice.reducer,
-    rooms: chatRoomsSlice.reducer
+    rooms: chatRoomsSlice.reducer,
+    [UserDataApi.reducerPath]:UserDataApi.reducer
   },
+  middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware().concat(UserDataApi.middleware)
 })
 
+setupListeners(store.dispatch)
+
+export * from './apis/UserDataApi'
 export { store }
