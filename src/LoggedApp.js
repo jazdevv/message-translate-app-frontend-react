5c30a00fa5ac7c36ed1886ef7e7f08f7a65e220b
Jazthedev <joelpepa2@gmail.com>
2023-03-12 15:34:19 +0100

    add get logguser profile data at first render

--- src/LoggedApp.js
+++ src/LoggedApp.js
@@ -2,8 +2,7 @@ import { useGetLoggUserDataQuery } from './store/index';
 import { Outlet } from "react-router-dom";
 
 function LoggedApp(){
-    const {data} = useGetLoggUserDataQuery();
-    console.log(data)
+    useGetLoggUserDataQuery();
     return <>
     <Outlet />
     </>
