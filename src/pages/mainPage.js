c859e2bbd7767e92c0094d393c89e2cb50f5a2dc
Jazthedev <joelpepa2@gmail.com>
2023-03-13 19:11:43 +0100

    add main page render

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,20 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { Link,Outlet } from "react-router-dom"
-
-
+import SideBarUsers from '../components/SideBarUsers';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useLoggUser } from '../hooks/useLoggUser';
+import { useDispatch } from 'react-redux';
+import { initialSetRooms } from '../store';
 
 function MainPage(){
+    const serverUrl = useSelector((state)=> state.serverUrl);
+    const rooms = useSelector((state)=> state.rooms);
+    const [loggUser] = useLoggUser();
+    const dispatch = useDispatch();
     
-    // ONLY FOR DEV
-    const tempChatRooms = <>
-        <Link to={'/chat/3'}><div className='p-5 border-4 border-indigo-500/100  cursor-pointer'>DEV USER1</div></Link>
-        <Link to={'/chat/1'}><div className='p-5 border-4 border-indigo-500/100 cursor-pointer'>DEV USER2</div></Link>
-    </>
+    useEffect(()=>{
+        const getRooms = async ()=> {
+            const {data} = await axios.get(`${serverUrl}/messages/rooms`, {withCredentials: true})
+            console.log("rooms",data)
+            dispatch(initialSetRooms(data));
+        }
+        
+        getRooms();
 
-    return <div className="w-24 flex flex-col">
-        {tempChatRooms}
-        { <Outlet /> }
-    </div>
+    },[])
+
+        
+    if(rooms.loading===true){
+        return 'Loading'
+    }else{
+      return <div className="flex">
+        <SideBarUsers rooms={rooms.rooms}/>
+        <Outlet /> 
+    </div>  
+    }
+    
 }// OUTLET === CHAT COMPONENT 
 
 export default MainPage
