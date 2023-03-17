44146349cefbfdcee650cdbacc11318d60d05024
Jazthedev <joelpepa2@gmail.com>
2023-03-17 15:13:25 +0100

    add component choose chatRoom

--- src/components/UserChatRoom.js
+++ src/components/UserChatRoom.js
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useChatRoomId } from "../hooks/useChatRoomId";
 import io from 'socket.io-client';
+import ConversationChat from "./Chat";
 
 
 const socket = io("ws://127.0.0.1:3001",{
@@ -13,32 +14,18 @@ function Chat(){
     //Chat roomid redux
     const {getChatRoomId: chatRoomid,setChatRoomid} = useChatRoomId();
     console.log("chatroom",chatRoomid)
-    //Actual conversation messages && other user
-    const [conversation,setConversation] = useState({
-        otheruser: undefined,
-        messages: undefined,
-        loading: true
-    })
+    
     //Allowed user roomid
-    const [Allowed,setAllowed] = useState(true)
+    const [Allowed,setAllowed] = useState(true);
 
     //Get URL params
-    let [searchParams] = useSearchParams()
+    let [searchParams] = useSearchParams();
 
     //Works as a piece state
-    const userid = searchParams.get('u')
-    const roomid = searchParams.get('r')
+    const userid = searchParams.get('u');
+    const roomid = searchParams.get('r');
 
-    //Render conversation every time is a different room id
-    const getMessages = useEffect(()=>{
-        //Return if not allowed user or chatRoomId not exists
-        if(Allowed===false||!chatRoomid){
-            return
-        }
-        //Get messages & other user
-        
-
-    },[chatRoomid]);
+    
 
     //join room every time url changes
     useEffect(()=>{
@@ -49,23 +36,26 @@ function Chat(){
     
     // listen for event that makes you get the validated roomcode from server
     useEffect(()=>{
+        
+        //SUCCES
         socket.on("resjoinroom",(data)=>{
             setAllowed(data.valid);
             setChatRoomid(data.roomid);
             
         })   
+        //FAIL
         socket.on("errjoinroom",(data)=>{
             if(data.valid===false){
                setAllowed(false);
             }
         }) 
-    },[])
+    },[userid,roomid])
 
     if(Allowed===false){
         return <div>You Shouldnt Be here</div>
     }else if(Allowed===true){
         if(chatRoomid){
-            return <div>{chatRoomid}</div>
+            return <ConversationChat socket={socket}/>
         }else if(!chatRoomid){
             return <div>Please choose a room</div>
         }    
