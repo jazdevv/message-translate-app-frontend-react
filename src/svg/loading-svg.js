efd81e58aad7c5279156a741f8f2d6e26fc5e4fd
Jazthedev <joelpepa2@gmail.com>
2023-03-22 21:44:55 +0100

    fix loading svg component not showing on some places

--- src/svg/loading-svg.js
+++ src/svg/loading-svg.js
@@ -1,6 +1,6 @@
 function LoadingSvg({type}){
     if(type ==='cubes'){
-      return <>
+      return <div>
                 <svg xmlns="http://www.w3.org/2000/svg"  className="animate-css" width="200px" height="200px" viewBox="0 0 100 100">
                     <rect x="17.5" y="30" width="15" height="40" fill="#93dbe9">
                         <animate attributeName="y" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="18;30;30" keySplines="0 0.5 0.5 1;0 0.5 0.5 1" begin="-0.2s"></animate>
@@ -15,7 +15,7 @@ function LoadingSvg({type}){
                         <animate attributeName="height" repeatCount="indefinite" dur="1s" calcMode="spline" keyTimes="0;0.5;1" values="58.00000000000001;40;40" keySplines="0 0.5 0.5 1;0 0.5 0.5 1"></animate>
                     </rect>
                 </svg>
-            </>  
+            </div>  
     }
     
 }
