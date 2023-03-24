fe8283b439709f12896333414ed481b1f24b2c20
Jazthedev <joelpepa2@gmail.com>
2023-03-24 18:15:36 +0100

    change styles

--- src/components/MessageOtherUser.js
+++ src/components/MessageOtherUser.js
@@ -3,7 +3,7 @@ function MessageOtherUser({message}){
     return  <div className="chat-message">
                 <div className="flex items-end">
                     <div className="flex flex-col space-y-2 text-md max-w-xs mx-2 order-2 items-start">
-                        <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">{message.text}</span></div>
+                        <div><span className="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-500 text-white">{message.text}</span></div>
                     </div>
                 </div>
             </div>
--- src/components/SideBarUser.js
+++ src/components/SideBarUser.js
@@ -10,7 +10,7 @@ function SideBarUser({name,profileImage,lastMessage,lastMessageDate}){
             </div>
             <div className="w-3/4 border-b border-gray-300 flex flex-col justify-center pb-1 hidden md:block ">
                 <div className="flex justify-between ">
-                    <div className="text-lg flex items-center font-bold">{name}</div>
+                    <div className="text-lg flex items-center text-xl font-medium text-gray-700 capitalize">{name}</div>
                     <div className="text-sm flex items-center text-gray-600">{lastMessageDate}</div>
                 </div>
                 <div className="text-gray-500">
