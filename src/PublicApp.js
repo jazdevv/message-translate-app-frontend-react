912953bfa9469115dec1b6ee2a3cd732a7d36fe0
Jazthedev <joelpepa2@gmail.com>
2023-03-26 13:28:06 +0200

    update publicapp component

--- src/PublicApp.js
+++ src/PublicApp.js
@@ -1,8 +1,6 @@
 import { Link,Outlet } from "react-router-dom"
 function PublicApp() {
     return <div>
-        <Link to={"/auth/login"}> <button>Login btn</button></Link>
-        <Link to={"/auth/signup"}><button>Signup btn</button></Link>
         <Outlet />
         </div>
 }
